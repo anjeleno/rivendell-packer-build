2f83263c4594813c9a0a7481c31b171a5ea33031
anjeleno <anjeleno@users.noreply.github.com>
2026-06-10 14:55:23 -0700

    Cleanup scratch files

--- patch_hcl.sh
+++
@@ -1,2 +0,0 @@
-sed -i '/"mkdir -p \/opt\/APPS"/a \      "echo '\''DPkg::Lock::Timeout \\"600\\";'\'' | sudo tee /etc/apt/apt.conf.d/99timeout",' /root/rivendell-cloud/rivendell.pkr.hcl
-sed -i '/"mkdir -p \/opt\/APPS"/a \      "sudo systemctl disable --now apt-daily.timer apt-daily-upgrade.timer || true",' /root/rivendell-cloud/rivendell.pkr.hcl
